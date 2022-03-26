7865bfa00b830fb66f530c333bbbfa82fde3c791
mrsid_18 <siddharthamehta18@gmail.com>
2022-03-26 12:16:54 -0700

    refactored code

--- responsiveVideo.js
+++ responsiveVideo.js
@@ -2,36 +2,35 @@ const vid = document.getElementById("bgVideo");
 const source = document.getElementById("bgVideoSource");
 const coverImg = document.getElementById("bgVideoCover");
 
-function setDesktopVideo(e) {
-    if (e.matches) changeSrc('video.mp4', 'https://i.stack.imgur.com/6M513.png')
-}
-
-function setMobileVideo(e) {
-    if (e.matches) changeSrc('video.mp4', 'https://i.stack.imgur.com/6M513.png')
-}
-
-function setTabletVideo(e) {
-    if (e.matches) changeSrc('mobile.mp4', 'https://i.stack.imgur.com/6M513.png')
-}
+const mediaQueries = [
+  {
+    query: "(min-width:1200px)",
+    coverImg: "https://i.stack.imgur.com/6M513.png",
+    videoSrc: "video.mp4",
+  },
+  {
+    query: "(min-width:577px) and (max-width:1199px)",
+    coverImg: "https://i.stack.imgur.com/6M513.png",
+    videoSrc: "mobile.mp4",
+  },
+  {
+    query: "(max-width:576px)",
+    coverImg: "https://i.stack.imgur.com/6M513.png",
+    videoSrc: "video.mp4",
+  },
+];
 
 function changeSrc(src, cover) {
-    source.setAttribute('src', src);
-    coverImg.setAttribute('src', cover);
-    vid.load();
-    vid.play();
+  source.setAttribute("src", src);
+  coverImg.setAttribute("src", cover);
+  vid.load();
+  vid.play();
 }
 
-
-const desktopDevice = window.matchMedia('(min-width:1200px)');
-const mobileDevice = window.matchMedia('(max-width:576px)');
-const tabletDevice = window.matchMedia('(min-width:577px) and (max-width:1199px)');
-
-//add event listeners when width changes
-desktopDevice.addEventListener('change', setDesktopVideo)
-mobileDevice.addEventListener('change', setMobileVideo)
-tabletDevice.addEventListener('change', setTabletVideo)
-
-//initial device check
-setDesktopVideo(desktopDevice)
-setMobileVideo(mobileDevice)
-setTabletVideo(tabletDevice)
+mediaQueries.forEach((mq) => {
+  let mediaQuery = window.matchMedia(mq.query);
+  mediaQuery.addEventListener("change", function (e) {
+    if (e.matches) changeSrc(mq.videoSrc, mq.coverImg);
+  });
+  if (mediaQuery.matches) changeSrc(mq.videoSrc, mq.coverImg);
+});
